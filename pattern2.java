f844f774915f119989b4930bd53b5ad922266560
Tamadajyothirmay <tamadajyothirmay2006@gmail.com>
2024-07-24 12:32:25 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,15 @@
+package pat;
+public class pattern2{
+   public static void main(String arg[]){
+   int rows=3;
+   for(int i=1;i<=rows;++i)
+   {
+    for(int j=1;j<=i;++j)
+    {
+       System.out.println("*");
+       }
+       System.out.println();
+    }
+    }
+    }
+     
